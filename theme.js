2690a2a3a4caa1b9f0d33b5d44b6fbc9fdaf4560
yuvanitha2006 <yuvanitha2006@gmail.com>
2026-01-06 17:31:12 +0530

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,18 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+
+    // Save preference
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+// Load saved theme on refresh
+window.onload = function () {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+};
